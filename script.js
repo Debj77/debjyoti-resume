da2a89cdf8c5c3d8d6b0d937b5c9218ff11dd4b7
Debj77 <124762887+Debj77@users.noreply.github.com>
2026-08-28 12:35:21 +0530

    Add download button event listener in script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('download-btn').addEventListener('click', function() {
+    console.log("Resume download started!");
+    alert("Thank you for downloading my resume!");
+});
